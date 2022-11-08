ab070e591d8ef09cfce0fd27a07a3011ee5edfc2
belisky <fiawornunobel@gmail.com>
2022-11-08 12:37:22 +0000

    chore:starter

--- weight_app/weight.py
+++ weight_app/weight.py
@@ -5,6 +5,10 @@ app=Flask(__name__)
 @app.route('/')
 def home():
     return ("hello green weight")
+
+@app.route('/weight')
+def get_weight():
+    return ("weight is green")
   
 if __name__=="__main__":
     app.run()
