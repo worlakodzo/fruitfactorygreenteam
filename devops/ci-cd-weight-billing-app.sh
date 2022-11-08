318264052ebe12ae757e004715e0566f4b32a31a
Worlako Dzokoto <worlaco.dzo@gmail.com>
2022-11-08 21:44:16 +0000

    Updated ci-cd script

--- devops/ci-cd-weight-billing-app.sh
+++ devops/ci-cd-weight-billing-app.sh
@@ -4,9 +4,24 @@
 # git pull -f origin main
 echo "Starting pulling green team weight and billing app repo"
 git pull -f origin main
-
-
 echo "Done pulling green team weight and billing app repo"
+
+
+
+function deploy_to_test(){
+
+}
+
+
+function run_test_script(){
+
+}
+
+
+function deploy_to_production(){
+
+}
+
 
 
 
