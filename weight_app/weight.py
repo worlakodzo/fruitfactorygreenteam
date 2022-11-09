f983b91fcf5e7f9d40f7b1b327aa01cc7877bc99
Samuel Ofori <oforisml@gmail.com>
2022-11-09 02:55:51 -0600

    Weight post at save

--- weight_app/weight.py
+++ weight_app/weight.py
@@ -1,5 +1,16 @@
 from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'weight_app'
+
+mysql = MySQL(app)
+
+
+
 
 @app.route("/weight", methods=["POST", "GET"])
 def get_weight():
@@ -9,11 +20,15 @@ def get_weight():
     if request.method=="POST":
         json_data = request.get_json()
         direction = json_data["direction"]
-        truck = json_data["license"]
+        license = json_data["license"]
         containers = json_data["containers"]
         weight = json_data["weight"]
         unit = json_data["unit"]
         force = json_data["force"]
+        if force == "true":
+            force='TRUE'
+        elif force=="false":
+            force='FALSE'
         produce=json_data["produce"]
 
         if direction == "IN":
@@ -22,8 +37,11 @@ def get_weight():
         elif direction == "OUT":
             neto = weight
 
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('INSERT INTO tbl_weight  (direction, license, containers, weight, unit, produce) VALUES(%s, %s, %s, %s, %s, %s)', (direction, license, containers, weight, unit, produce))        
+        mysql.connection.commit()
 
-        data.extend((truck, direction, containers, unit, force, produce))
+        data.extend((license, direction, containers, unit, force, produce))
         
          # Data structure of JSON format
         _data = jsonify(data) # Converts your data strcuture into JSON format
