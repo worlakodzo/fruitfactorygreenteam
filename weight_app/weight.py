9bd1f134951b64996da0295a418f253c61a7ca1f
Samuel Ofori <oforisml@gmail.com>
2022-11-09 15:26:49 -0600

    /batch-weight

--- weight_app/weight.py
+++ weight_app/weight.py
@@ -1,6 +1,11 @@
+#IMPORTS
 from flask import Flask, request, jsonify
+from datetime import datetime
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
+import csv
+
+
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -17,43 +22,61 @@ def get_weight():
     data = []
     bruto = None
     neto = None
+    resp=None
     if request.method=="POST":
         json_data = request.get_json()
         direction = json_data["direction"]
         license = json_data["license"]
+        if len(license)==0:
+            license = "na"
         containers = json_data["containers"]
         weight = json_data["weight"]
         unit = json_data["unit"]
         force = json_data["force"]
-        if force == "true":
-            force='TRUE'
-        elif force=="false":
-            force='FALSE'
+   
         produce=json_data["produce"]
+
+        # Date and time of saving the weight data
+        date=datetime.now()
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('INSERT INTO tbl_weight  (direction, license, containers, weight, unit, produce, forced, date) VALUES(%s, %s, %s, %s, %s, %s,%s,%s)', (direction, license, containers, weight, unit, produce, force, date))    
+        record_id = cursor.lastrowid
+        mysql.connection.commit()
 
         if direction == "IN":
             bruto = weight
+            resp = { "id": record_id, "truck": license, "bruto": bruto }
 
         elif direction == "OUT":
             neto = weight
-
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('INSERT INTO tbl_weight  (direction, license, containers, weight, unit, produce) VALUES(%s, %s, %s, %s, %s, %s)', (direction, license, containers, weight, unit, produce))        
-        mysql.connection.commit()
-
-        data.extend((license, direction, containers, unit, force, produce))
+            resp = { "id": record_id, "truck": license, "neto": neto }
         
          # Data structure of JSON format
-        _data = jsonify(data) # Converts your data strcuture into JSON format
+        reponse = jsonify(resp) # Converts your data strcuture into JSON format
         
        
-        _data.status_code = 202 # Provides a response status code of 202 which is "Accepted" 
+        reponse.status_code = 202 # Provides a response status code of 202 which is "Accepted" 
 
-        return _data # Returns the HT
+        return reponse # Returns the HT
 
     elif request.method=="GET":
         return "<h1>weight<h1>"
     return 404
+
+@app.route("/batch-weight/<file_name>", methods=["POST"])
+def get_batch_weight(file_name):
+   
+    if file_name.lower().endswith('.csv'):
+         with open(f"./Samples/{file_name}", 'r') as file:
+            reader = csv.DictReader(file)
+            for i in reader:
+                print(i)
+    elif file_name.lower().endswith('.json'):
+        return None
+
+    return "M"
+
 if __name__=="__main__":
     app.run()
 
