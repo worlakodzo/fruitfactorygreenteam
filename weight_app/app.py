a6b69384986dec130e1b1dc5a924ccbb135facf9
belisky <denoblesnobility2@gmail.com>
2022-11-10 13:47:24 +0000

    fix: fixed return format

--- weight_app/app.py
+++ weight_app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask,request,jsonify,Response
+from flask import Flask,request,jsonify
 from flask_mysqldb import MySQL
 import datetime
+ 
  
  
 app=Flask(__name__)
@@ -10,6 +11,7 @@ app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']='passwd'
 app.config['MYSQL_DB']='weight'
+app.config['MYSQL_CURSORCLASS']='DictCursor'
 
 mysql=MySQL(app)
 @app.route('/')
@@ -19,32 +21,25 @@ def home():
 @app.route('/session/<id>')
 def get_session(id):     
     id=int(id)
-    temp_dict={}     
+     
     cur=mysql.connection.cursor()
     results=cur.execute("SELECT direction,id,truck,bruto,truckTara,neto FROM transactions WHERE id LIKE %s",(id,))
     if results>0:
-        session_details=cur.fetchall()
-        if session_details[0][0]=='in':
-            temp_dict['id']=session_details[0][1]
-            temp_dict['truck']=session_details[0][2]
-            temp_dict['bruto']=session_details[0][3]
-        if session_details[0][0]=='out':
-            temp_dict['id']=session_details[0][1]
-            temp_dict['truck']=session_details[0][2]
-            temp_dict['bruto']=session_details[0][3]
-            temp_dict['truckTara']=session_details[0][4]
-            temp_dict['neto']=session_details[0][5]   
-    # for item in results:
-    #     temp_dict[item]
- 
-    return temp_dict,201
+        session_details=cur.fetchall() 
+  
+        resp=jsonify(session_details)
+        resp.status_code=200
+        return resp
+    resp=jsonify({"message":"session not found"})
+    resp.status_code=404
+    return resp
         
 @app.route('/weight')
 def get_weight():
     begin='0000-00-00 00:00:00.000000'
     end=datetime.datetime.now() 
     fil=''
-    cur=mysql.connection.cursor()
+    cur=mysql.connection.cursor()     
    
     if request.args.get('filter'):
         if request.args.get('from'):
@@ -54,26 +49,23 @@ def get_weight():
         fil=request.args.get('filter')             
         results=cur.execute("SELECT id,direction,bruto,neto,produce,containers FROM transactions WHERE direction LIKE %s AND datetime BETWEEN %s AND %s",(fil,begin,end))
         if results>0:         
-            transanction_Details=cur.fetchall()
-            final_output=[]
-            for row in transanction_Details:
-                dict=row_to_dict(row)
-                final_output.append(dict)              
+            transanction_Details=cur.fetchall()               
+            resp=jsonify(transanction_Details)
+            resp.status_code=200
  
-            return final_output,201
+            return resp
     else:         
         results=cur.execute("SELECT id,direction,bruto,neto,produce,containers FROM transactions WHERE datetime BETWEEN %s AND %s  ORDER BY direction",(begin,end))    
         if results>0:                   
-            transanction_Details=cur.fetchall()
-            final_output=[]
-            for row in transanction_Details:
-                dict=row_to_dict(row)
-                final_output.append(dict)
-     
-            return final_output,201
+            transanction_Details=cur.fetchall() 
+            resp=jsonify(transanction_Details)
+            resp.status_code=200 
+            return resp
+ 
+    resp=jsonify({"message":"session not found"})
+    resp.status_code=404
+    return resp
 
-            
-    return ("weight is empty")
 def row_to_dict(row):
     temp_dict_list=['id','direction','bruto','neto','produce','containers']
     temp_dict={}
