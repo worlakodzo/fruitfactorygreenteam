dc1ed01af3fb7d2e186be7bb5af737817e212d4a
Christopher Afeku junior <95024781+DO-Christopher@users.noreply.github.com>
2022-11-10 21:11:33 +0000

    minor fixes

--- devops/health_monitor.sh
+++ devops/health_monitor.sh
@@ -7,11 +7,11 @@ function health_check(){
     # checking weight endpoint health status
     nc -zv $weight_endpoint 8081 > temp.txt 2>&1
     cat temp.txt | xargs | awk '{ "date" | getline d; print d " "$0}' >> weight_app_logs.txt
-    sleep 5
 
     # checking billing endpoint health status
-    nc -zv $weight_endpoint 8081 > temp.txt 2>&1
+    nc -zv $weight_endpoint 8082 > temp.txt 2>&1
     cat temp.txt | xargs | awk '{ "date" | getline d; print d " "$0}' >> weight_app_logs.txt
+
     sleep 5
 }
 
