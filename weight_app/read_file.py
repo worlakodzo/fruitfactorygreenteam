ef904ad2da650ab3b9c37bfe48388ba173a2acaa
Samuel Ofori <oforisml@gmail.com>
2022-11-11 09:13:14 -0600

    changed db table to match requirement

--- weight_app/read_file.py
+++ weight_app/read_file.py
@@ -2,39 +2,35 @@ from flask_mysqldb import MySQL
 import MySQLdb.cursors
 
 
-
-
-
 def save_container_record(c_id, c_weight, c_unit):
-    qry = ("INPUT INTO containers registered(container_id, weight, unit) values(%s, %s, %s,)", c_id, c_weight, c_unit)
+    qry = ("INPUT INTO containers_registered(container_id, weight, unit) values(%s, %s, %s,)",
+           c_id, c_weight, c_unit)
     return qry
-
 
 
 def read_file_save(a, ext):
     sum = 0
     unit = None
-    
-    #Reading csv
-    if ext=="csv":
+
+    # Reading csv
+    if ext == "csv":
         # When unit is in kg
-        try: 
+        try:
             for i in a:
                 # save_container_record(i)
                 print(i)
-                sum+=int(i['kg'])
+                sum += int(i['kg'])
             unit = 'kg'
         # When unit is in lbs
         except:
             for i in a:
                 print(i)
-                sum+=int(i['lbs'])
-            unit='lbs'
+                sum += int(i['lbs'])
+            unit = 'lbs'
         return sum, unit
-    # Reading json file    
-    elif ext=="json":
+    # Reading json file
+    elif ext == "json":
         for i in a:
-            sum+=int(i['weight'])
+            sum += int(i['weight'])
         unit = i['unit']
         return sum, unit
-
