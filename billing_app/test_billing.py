06a21373972f8a13be42bb3ad8ce921aff35b7d9
Noah Ekow Danquah Abekah <abekah.ekow@gmail.com>
2022-11-12 23:20:22 +0000

    changes to test script file

--- billing_app/test_billing.py
+++ billing_app/test_billing.py
@@ -124,20 +124,3 @@ def test_post__truck_201():
 
 
 
-
-
-
-
-# def test_get_locations_for_us_90210_check_content_type_equals_json():
-#      response = requests.get("http://api.zippopotam.us/us/90210")
-#      assert response.headers["Content-Type"] == "application/json"
-
-# def test_get_locations_for_us_90210_check_country_equals_united_states():
-#      response = requests.get("http://api.zippopotam.us/us/90210")
-#      response_body = response.json()
-#      assert response_body["country"] == "United States"
-
-# def test_get_locations_for_us_90210_check_city_equals_beverly_hills():
-#     response = requests.get("http://api.zippopotam.us/us/90210")
-#     response_body = response.json()
-#     assert response_body["places"][0]["place name"] == "Beverly Hills"
